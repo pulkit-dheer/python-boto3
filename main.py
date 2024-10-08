38c8bd2227a8061e56f52b9be20a5c185c8e2d20
Pulkit Dheer <pulkitkdheer@gmail.com>
2024-10-08 20:08:53 +0530

    vpc creation

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
 import boto3
 
+
+ec2_client = boto3.client('ec2', region_name="ap-south-1")
+ec2_resource = boto3.resource('ec2', region_name="ap-south-1")
+
+new_vpc = ec2_resource.create_vpc(
+    CidrBlock="10.0.0.0/16"
+)
+
+
+new_vpc.create_subnet(
+    CidrBlock="10.0.1.0/24"
+)
+
+new_vpc.create_subnet(
+    CidrBlock="10.0.2.0/24"
+)
+
+
+
+new_vpc.create_tags(
+    Tags=[
+        {
+            'Key':'Name',
+            'Value':'my-vpc'
+        }
+    ]
+)
+
+
+all_available_vpcs = ec2_client.describe_vpcs()
+
+vpcs = all_available_vpcs["Vpcs"]
+
+for vpc in vpcs:
+    print(vpc["VpcId"])
+    cidr_block_assoc_sets = (vpc["CidrBlockAssociationSet"])
+    for asso_set in cidr_block_assoc_sets:
+        print(asso_set["CidrBlockState"])
